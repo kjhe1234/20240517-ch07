449361fe3a014a709d585c334a5fdea13f87046c
kjhe1234 <kjhe1234@naver.com>
2024-05-17 14:21:13 +0900

    게터 세터 연습 추가

--- src/com/kjhe1234/member/MainClass_s.java
+++ src/com/kjhe1234/member/MainClass_s.java
@@ -6,21 +6,26 @@ public class MainClass_s {
 		// TODO Auto-generated method stub
 		Member_s member1 = new Member_s();
 		member1.setId("tiger");
-//		member1.id = "tiger";
-//		member1.pw = "12345";
-//		member1.name = "홍길동";
-//		member1.email = "hong@abc.com";
-//		member1.age = 27;
+		member1.setPw("12345");
+		member1.setName("홍길동");
+		member1.setEmail("hong@abc.com"); 
+		member1.setAge( 27);
 		
 		Member_s member2 = new Member_s();
-//		member2.id = "lion";
-//		member2.pw = "12345";
-//		member2.name = "이순신";
-//		member2.email = "lee@abc.com";
-//		member2.age = 32;
+		member2.setId("lion");
+		member2.setPw("12345");
+		member2.setName("이순신");
+		member2.setEmail("lee@abc.com"); 
+		member2.setAge( 32);
+
 		
-	System.out.println("회원1이름:"+member1.getId());  // 홍길동
-//		System.out.println("회원2이름:"+member2.name);  // 이순신
+		Member_s member3 = new Member_s("whitecat","8888", "김유신", "white@abc.com", 41);
+		// 생성자를 사용하여 필드값이 초기화된 객체 생성
+		
+	System.out.println("회원1이름:"+member1.getName());  // 홍길동
+	System.out.println("회원1아이디:"+member1.getId());  
+	System.out.println("회원2이름:"+member2.getName()); // 이순신
+	System.out.println("회원3 이메일"+member3.getEmail());
 	}
 
 
